85e84c38ae961139a236bdd528c756e64071feb4
Benjamin W. Zale <benjamin.zale@gmail.com>
2020-05-31 09:52:40 -0400

    Only allow one instance of this application

--- MasterMike/Program.cs
+++ MasterMike/Program.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MasterMike
 {
+    /// <summary>
+    /// The main class of the application.
+    /// </summary>
     static class Program
     {
         /// <summary>
@@ -14,10 +18,21 @@ namespace MasterMike
         [STAThread]
         static void Main()
         {
+            using var mutex = new Mutex(true, "UniqueAppId", out bool isAnotherInstanceRunning);
+
+            // Another instance is already running.
+            if (!isAnotherInstanceRunning)
+            {
+                return;
+            }
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            // Ensure mutex is not GC'ed.
+            GC.KeepAlive(mutex);
         }
     }
 }
